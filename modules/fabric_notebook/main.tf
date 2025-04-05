d6160b1180d354eaf78067eb7a727fde8ce888f0
JannoCAT <janno@cloudandthings.io>
2025-04-05 12:25:53 +0200

    fix: 2025-04-05 Ensure changes to notebooks are made

--- modules/fabric_notebook/main.tf
+++ modules/fabric_notebook/main.tf
@@ -2,10 +2,10 @@ resource "fabric_notebook" "notebook" {
   display_name              = var.display_name
   description               = var.description
   workspace_id              = var.workspace_id
-  definition_update_enabled = false
+  definition_update_enabled = true
   format                    = "ipynb"
   definition = {
-    "notebook-content.ipynb" = {
+    "notebook-content.ipynb"  = {
       source = var.local_file_path
     }
   }
