68fe8eb6055d4802dc118344ee0e51276dfcfa15
JannoCAT <130350740+JannoCAT@users.noreply.github.com>
2026-03-26 13:53:45 +0200

    chore: 2026-03-26 Update fabric provider version 1.8.0 (#4)

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     fabric = {
       source  = "microsoft/fabric"
-      version = "1.4.0"
+      version = "1.8.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
