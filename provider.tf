36a71c50a68ddfffd1cfaf144ef5df5eece86c36
JannoCAT <janno@cloudandthings.io>
2026-04-24 15:52:49 +0200

    feat: 2026-4-24 Add new Features

--- provider.tf
+++ provider.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     fabric = {
       source  = "microsoft/fabric"
+<<<<<<< HEAD
       version = "1.8.0"
+=======
+      version = "1.6.0"
+>>>>>>> b07b7af (feat: 2026-4-24 Add new Features)
     }
     azurerm = {
       source  = "hashicorp/azurerm"
